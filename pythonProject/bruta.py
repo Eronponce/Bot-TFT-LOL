2bb16f2fc922b540638c8fab41da623e5135b332
Eron <81422635+Eronponce@users.noreply.github.com>
2022-09-24 11:18:51 -0300

    coisass

--- pythonProject/bruta.py
+++ pythonProject/bruta.py
@@ -9,10 +9,16 @@ ativo = True
 Loop = True
 pg.PAUSE = 0.1
 def comp():
+    print("Inicio")
+
     for i in range(2):
+        print("Final")
         coordinates = pg.locateOnScreen('Malphite.png', region=regiao)
+        print(regiao)
+        print(coordinates)
         if(coordinates != None):
             pg.moveTo(coordinates[0], coordinates[1],_pause=False)
+            print("malph")
             mouse.click()
 
         coordinates = pg.locateOnScreen('Skarner.png', region=regiao)
@@ -39,21 +45,23 @@ def comp():
             mouse.click()
 
         coordinates = pg.locateOnScreen('Sylas.png', region=regiao)
+        print(regiao)
+        print(coordinates)
         if (coordinates != None):
             pg.moveTo(coordinates[0], coordinates[1],_pause=False)
             mouse.click()
 
 def compra():
     comp()
-    pg.moveTo(362, 990,_pause=False)
+    pg.moveTo(comprareal,_pause=False)
     print("feito")
     mouse.click()
 
 
 # def dinheiro():
-    # image =  pg.screenshot(region=(340, 980, 25, 25))
-    # image.save("screenshot.png")
-    # print(textract.process("screenshot.png"))
+#  image   print(textract.process("screenshot.png"))
+
+
 def definirRegiao1():
     comeco = []
     while True:
@@ -71,12 +79,22 @@ def definirRegiao2():
             break
 
     return final
+def definirCompra():
+    compra= []
+    while True:
+        if keyboard.is_pressed("e"):
+            compra = pg.position()
+            print("Feito 3")
+            break
+    return compra
+
 #Defini a regiao que irá interagir
 comecoreal = definirRegiao1()
 finalreal = definirRegiao2()
-regiao = (comecoreal[0],comecoreal[1], finalreal[0] -comecoreal[0] , finalreal[1] -comecoreal[1])
-
-while(ativo):
+comprareal = definirCompra()
+regiao = (comecoreal[0],comecoreal[1], finalreal[0] - comecoreal[0] , finalreal[1] -comecoreal[1])
+print(regiao)
+while True:
     if(keyboard.read_key() == 'y'):
         compra()
     if(keyboard.read_key() == 'i'):
